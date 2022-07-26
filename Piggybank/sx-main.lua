e0fab2eb01908b9469ab37bf1cbafbae3605d19e
gasiguus <88008999+gasiguus@users.noreply.github.com>
2022-07-27 02:44:40 +0300

    Add files via upload

---
+++ Piggybank/sx-main.lua
@@ -0,0 +1,16 @@
+ESX = nil 
+
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+ESX.RegisterUsableItem('piggybank', function(source)
+
+local xPlayer = ESX.GetPlayerFromId(source)
+local summa = math.random(0,2500)
+
+xPlayer.removeInventoryItem('piggybank', 1)
+xPlayer.addMoney(summa)
+if summa > 0 then
+ESX.ShowNotification('You examined the wallet and found ~g~'..summa..' €')
+elseif summa == 0 then
+ESX.ShowNotification('You examined the wallet which was empty. You found ~g~0€')
+end
+end)
