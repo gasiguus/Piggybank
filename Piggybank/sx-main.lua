e6dfa3ec9ecda3f3491ebf701cce1f41f7322b88
gasiguus <88008999+gasiguus@users.noreply.github.com>
2022-10-28 12:53:21 +0300

    Update sx-main.lua

--- Piggybank/sx-main.lua
+++ Piggybank/sx-main.lua
@@ -9,8 +9,8 @@ local summa = math.random(0,2500)
 xPlayer.removeInventoryItem('piggybank', 1)
 xPlayer.addMoney(summa)
 if summa > 0 then
-ESX.ShowNotification('You examined the wallet and found ~g~'..summa..' €')
+ESX.ShowNotification('You examined the piggybank and found ~g~'..summa..' €')
 elseif summa == 0 then
-ESX.ShowNotification('You examined the wallet which was empty. You found ~g~0€')
+ESX.ShowNotification('You examined the piggybank which was empty. You found ~g~0€')
 end
 end)
